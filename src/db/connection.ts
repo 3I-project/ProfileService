1422e2a16b9fc2f457979fe8927ce9bcc71d7ff5
TankistPro <nakol.nayk@mail.ru>
2022-05-04 19:48:28 +0300

    fix ssl

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -6,8 +6,8 @@ export const db = new Pool({
     database: process.env["DB_DATABASE"],
     password: process.env["DB_PASSWORD"],
     port: Number(process.env["DB_PORT"]),
-    ssl: {
-        rejectUnauthorized: false
-    }
+    // ssl: {
+    //     rejectUnauthorized: false
+    // }
 });
 
